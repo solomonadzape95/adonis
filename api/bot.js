dcab915ce57e46df67a3a95ffbc2c276f09a3577
Solomon Adzape <adzapesolomon@gmail.com>
2024-09-13 13:30:34 +0100

    edits

--- api/bot.js
+++ api/bot.js
@@ -1,5 +1,7 @@
 import { Bot, webhookCallback } from "grammy";
-
+export const config = {
+  runtime: "edge",
+};
 const token = process.env.BOT_TOKEN;
 if (!token) throw new Error("BOT_TOKEN is unset");
 
